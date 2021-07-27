1a791dc8d19e0d0d251776446842fea4a52f28f0
Rasmushibra <73135731+Rasmushibra@users.noreply.github.com>
2021-07-27 15:07:19 +0200

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <button :style="{background: color}" class="btn">
+        {{text}}
+    </button>
+</div>
+    
+</template>
+
+<script>
+
+export default {
+        name: 'Button',
+        props:{
+            text: String,
+            color: String
+        }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button />
+        <Button text="Add Task" color="green" />
+        <Button text="Update Task" color="blue" />
+        <Button text="Delete Task" color="red" />
+    </header>
+</div>
+    
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
